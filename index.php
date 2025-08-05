2e4e955be0651e3b1262b318642be3a126d52db1
BOH DZOMO <bohdzomobobpeter@gmail.com>
2025-08-05 10:42:59 +0100

    initial setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$title = "Todo Site";
+require_once("./includes/header.inc.php");
+?>
+<title>Todo Site</title>
+<link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+   <main>
+    
+
+   </main>
+<script src="./assets/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+<script src="./assets/Vendor/fontawesome/js/all.min.js"></script>
+</body>
+</html>
